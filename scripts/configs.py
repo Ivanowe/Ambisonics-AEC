a2013a0df599d7e3f804f73600ab1ec5057ba10d
joott002 <johann.ott002@stud.fh-dortmund.de>
2024-02-11 22:18:20 +0100

    adjusted hop size

--- scripts/configs.py
+++ scripts/configs.py
@@ -1,8 +1,7 @@
-
-
 exp_conf = {
     'in_norm': False, # normalize the input audio
     'sample_rate': 16000,
     'win_len': 0.020, # window length (sec)
-    'hop_len': 0.010  # window shift (sec)
+    # shorter hop -> 75% overlap
+    'hop_len': 0.005  # window shift (sec)
 }
