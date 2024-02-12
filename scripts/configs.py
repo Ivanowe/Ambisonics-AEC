f45fa4ee70f6f8fe5023c1db062fa8978db082e1
joott002 <johann.ott002@stud.fh-dortmund.de>
2024-02-12 06:06:30 +0100

    added config

--- scripts/configs.py
+++ scripts/configs.py
@@ -2,6 +2,13 @@ exp_conf = {
     'in_norm': False, # normalize the input audio
     'sample_rate': 16000,
     'win_len': 0.020, # window length (sec)
+    'hop_len': 0.010,  # window shift (sec)
+}
+amb_aec_conf = {
+    'in_norm': False, # normalize the input audio
+    'sample_rate': 16000,
+    'win_len': 0.020, # window length (sec)
     # shorter hop -> 75% overlap
-    'hop_len': 0.005  # window shift (sec)
+    'hop_len': 0.005,  # window shift (sec)
+    'num_channels': 5 # number of input channels
 }
