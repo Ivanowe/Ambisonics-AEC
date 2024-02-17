f6343f15fb03d37584c57d3d46f973c054875c8f
joott002 <johann.ott002@stud.fh-dortmund.de>
2024-02-17 03:17:24 +0100

    debugged loss function

--- scripts/utils/criteria.py
+++ scripts/utils/criteria.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 class LossFunction(object):
-    # old version kept for reference
+    #old version kept for reference
     # def __call__(self, est, lbl, loss_mask, n_frames):
     #     est *= loss_mask
     #     lbl *= loss_mask
@@ -13,21 +13,25 @@ class LossFunction(object):
         
     #     return loss
     
-    def __call__(self, est, lbl, loss_mask, n_out_channels):
-        est *= loss_mask
-        lbl *= loss_mask
+    def __call__(self, est, lbl, loss_mask, n_frames, n_out_channels):
+       # est *= loss_mask
+       # lbl *= loss_mask
+        
+        n_feats = est.shape[-1]
         
         # output channels are interleaved real and imaginary parts of the 
         # spectrum. The real part is therefore at even indeces and the imaginary 
         # part is at the odd indeces.
-        est_real = torch.stack([est[:, i :, :] for i in range(0, n_out_channels, 2)], dim=1)
-        est_imag = torch.stack([est[:, i :, :] for i in range(1, n_out_channels, 2)], dim=1)
+        est_real = torch.cat([est[:, i :, :] for i in range(0, n_out_channels * 2, 2)], dim=0)
+        est_imag = torch.cat([est[:, i :, :] for i in range(1, n_out_channels * 2, 2)], dim=0)
         
-        lbl_real = torch.stack([lbl[:, i :, :] for i in range(0, n_out_channels, 2)], dim=1)
-        lbl_imag = torch.stack([lbl[:, i :, :] for i in range(1, n_out_channels, 2)], dim=1)
+        lbl_real = torch.cat([lbl[:, i :, :] for i in range(0, n_out_channels * 2, 2)], dim=0)
+        lbl_imag = torch.cat([lbl[:, i :, :] for i in range(1, n_out_channels * 2, 2)], dim=0)
         
-        loss = (0.5 * torch.sum(np.absolute(est_real) - np.absolute(lbl_real))**2 
-                + 0.5 * torch.sum((np.absolute(est_imag) - np.absolute(lbl_imag))**2))
+        loss = (0.5 * torch.sum(torch.abs(est_real) - torch.abs(lbl_real))**2 
+                + 0.5 * torch.sum((torch.abs(est_imag) - torch.abs(lbl_imag))**2))
+        
+        #loss = loss / float(sum(n_frames) * n_feats * 2)
             
         return loss
 
