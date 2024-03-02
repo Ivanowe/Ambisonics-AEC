a3537f89bf9887dc796d661df3fbc2ce219e36ee
joott002 <johann.ott002@stud.fh-dortmund.de>
2024-03-03 00:48:46 +0100

    adjusted loss function

--- scripts/utils/criteria.py
+++ scripts/utils/criteria.py
@@ -13,23 +13,36 @@ class LossFunction(object):
         
     #     return loss
     
-    def __call__(self, est, lbl, loss_mask, n_out_channels):
+    def __call__(self, est, lbl, loss_mask, n_out_channels, n_frames):
         est *= loss_mask
         lbl *= loss_mask
         
+        n_feats = est.shape[-1]
+      
         # output channels are interleaved real and imaginary parts of the 
         # spectrum. The real part is therefore at even indeces and the imaginary 
         # part is at the odd indeces.
-        est_real = torch.cat([est[:, i :, :] for i in range(0, n_out_channels * 2, 2)], dim=0)
-        est_imag = torch.cat([est[:, i :, :] for i in range(1, n_out_channels * 2, 2)], dim=0)
+        est_real = torch.cat(
+            [est[:, i :, :] for i in range(0, n_out_channels * 2, 2)], 
+            dim=0)
+        est_imag = torch.cat(
+            [est[:, i :, :] for i in range(1, n_out_channels * 2, 2)], 
+            dim=0)
         
-        lbl_real = torch.cat([lbl[:, i :, :] for i in range(0, n_out_channels * 2, 2)], dim=0)
-        lbl_imag = torch.cat([lbl[:, i :, :] for i in range(1, n_out_channels * 2, 2)], dim=0)
+        lbl_real = torch.cat(
+            [lbl[:, i :, :] for i in range(0, n_out_channels * 2, 2)],
+            dim=0)
+        lbl_imag = torch.cat(
+            [lbl[:, i :, :] for i in range(1, n_out_channels * 2, 2)], 
+            dim=0)
         
-        loss = (0.5 * torch.sum(torch.abs(est_real) - torch.abs(lbl_real))**2 
-                + 0.5 * torch.sum((torch.abs(est_imag) - torch.abs(lbl_imag))**2))
+        loss = (0.5 
+                * torch.sum(torch.abs(est_real) 
+                            - torch.abs(lbl_real))**2 
+                + 0.5 * torch.sum((torch.abs(est_imag) 
+                                   - torch.abs(lbl_imag))**2))
         
-        #loss = loss / float(sum(n_frames) * n_feats * 2)
+        loss = loss / float(sum(n_frames) * n_feats * 2)
             
         return loss
 
