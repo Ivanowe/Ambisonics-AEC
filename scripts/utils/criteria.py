161fa06a41318f40125a806f2ce91c8e7a69d708
joott002 <johann.ott002@stud.fh-dortmund.de>
2024-03-12 10:42:34 +0100

    Refactor loss function to simplify code & to make it actaully work

--- scripts/utils/criteria.py
+++ scripts/utils/criteria.py
@@ -22,23 +22,13 @@ class LossFunction(object):
         # output channels are interleaved real and imaginary parts of the 
         # spectrum. The real part is therefore at even indeces and the imaginary 
         # part is at the odd indeces.
-        est_real = torch.cat(
-            [est[:, i :, :] for i in range(0, n_out_channels * 2, 2)], 
-            dim=0)
-        est_imag = torch.cat(
-            [est[:, i :, :] for i in range(1, n_out_channels * 2, 2)], 
-            dim=0)
+        est_real = est[:, ::2, :, :]
+        est_imag = est[:, 1::2, :, :]
+        lbl_real = lbl[:, ::2, :, :]
+        lbl_imag = lbl[:, 1::2, :, :]
         
-        lbl_real = torch.cat(
-            [lbl[:, i :, :] for i in range(0, n_out_channels * 2, 2)],
-            dim=0)
-        lbl_imag = torch.cat(
-            [lbl[:, i :, :] for i in range(1, n_out_channels * 2, 2)], 
-            dim=0)
-        
-        loss = (0.5 
-                * torch.sum(torch.abs(est_real) 
-                            - torch.abs(lbl_real))**2 
+        loss = (0.5 * torch.sum(torch.abs(est_real)  
+                                - torch.abs(lbl_real))**2 
                 + 0.5 * torch.sum((torch.abs(est_imag) 
                                    - torch.abs(lbl_imag))**2))
         
